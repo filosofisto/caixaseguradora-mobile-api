a32d58b714de49fcc0d848db5d24471dbdcaf839
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2018-12-25 21:14:40 -0200

    MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_12_25_164950) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
